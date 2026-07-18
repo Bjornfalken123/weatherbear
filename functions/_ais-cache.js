6fbe2be2c6eb3d1d3aadfaf30282e68d7a27bd49
Bjornfalken123 <bjorn.falkenang@gmail.com>
2026-07-18 14:06:20 +0200

    Add files via upload

--- functions/_ais-cache.js
+++ functions/_ais-cache.js
@@ -4,7 +4,7 @@ const AIS_VISIBLE_MAX_AGE_MS = 60 * 60 * 1000;
 const AIS_CACHE_REGIONS = [
   {
     id: "sweden-west",
-    name: "Orust med omnejd",
+    name: "Svenska västkusten",
     bbox: [10.0, 55.0, 13.2, 59.5]
   },
   {
